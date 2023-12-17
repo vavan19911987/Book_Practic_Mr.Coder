e12383491f81d3675271c369afa8bd704e31ba9c
Vavan19911987 <vavan7520@gmail.com>
2023-12-17 18:59:13 +0500

    Игра в города на JavaScript исправлнен БАГ  с одинаковыми буквами с начала и кончца города

--- Practice_Book/2.Practice_CityGame/js/script.js
+++ Practice_Book/2.Practice_CityGame/js/script.js
@@ -16,36 +16,39 @@
 // * и ход переходит к следующему игроку. Если же какое-то условие не выполняется, то нужно
 // * вывести сообщение об этом.
 
-let field = document.querySelector('#field');
-let message = document.querySelector('#message');
-let text = document.querySelector('#text')
-let cities = [];
-field.addEventListener("keypress", (e) => {
-    if (e.which === 13) {
-        if (cities.length === 0) {
-            cities.push(field.value);
-            console.log(cities)
-            text.textContent = cities[cities.length -1].slice(-1)
-        }
-        if (cities.length > 0) {
-            for (const city of cities) {
-                if (cities[cities.length -1].slice(-1) === field.value.slice(0, 1)) {
-                    cities.push(field.value);
-                    console.log(cities)
-                    text.textContent = cities[cities.length -1].slice(-1)
-                }
+;(function (root) {
+    let parent = document.querySelector(root)
+    let field = parent.querySelector('#field');
+    let message = parent.querySelector('#message');
+    let text = parent.querySelector('#text')
+    let cities = [];
+    field.addEventListener("keypress", (e) => {
+        if (e.which === 13) {
+            if (cities.length === 0) {
+                cities.push(field.value);
+                console.log(cities)
+                text.textContent = cities[cities.length -1].slice(-1)
             }
-            cities.filter((el, i , arr) => {
-                if (arr.indexOf(el) !== i) {
-                    message.textContent = 'Город уже был назван'
-                    cities.pop()
-                    text.textContent = cities[cities.length -1].slice(-1)
-                } else {
-                    message.textContent = '';
+            if (cities.length > 0) {
+                for (const city of cities) {
+                    if (cities[cities.length -1].slice(-1) === field.value.slice(0, 1)) {
+                        cities.push(field.value);
+                        console.log(cities)
+                        text.textContent = cities[cities.length -1].slice(-1)
+                        break;
+                    }
                 }
-            })
+                cities.filter((el, i , arr) => {
+                    if (arr.indexOf(el) !== i) {
+                        cities.pop()
+                        message.textContent = 'Город уже был назван'
+                        text.textContent = cities[cities.length -1].slice(-1)
+                    } else {
+                        message.textContent = '';
+                    }
+                })
+            }
+            field.value = '';
         }
-        field.value = '';
-    }
-})
-
+    })
+})('.sectionGame' );
