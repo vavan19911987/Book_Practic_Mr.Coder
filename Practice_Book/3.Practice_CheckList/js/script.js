cef5fe2484fef9a319139e5f741075f90ce64c37
Vavan19911987 <vavan7520@gmail.com>
2024-05-22 00:05:59 +0500

    чÐек лист

--- Practice_Book/3.Practice_CheckList/js/script.js
+++ Practice_Book/3.Practice_CheckList/js/script.js
@@ -10,20 +10,33 @@ input.addEventListener('keypress', function(event) {
         let task = document.createElement('span');
         task.classList.add('task');
         task.textContent = this.value;
+        task.addEventListener('dblclick', function(ev) {
+            let inp = document.createElement('input');
+            inp.value = task.textContent
+            task.textContent = '';
+            task.appendChild(inp);
+            if (ev.key === 'Enter') {
+                inp.remove();
+                task.textContent = inp.textContent;
+            }
+        });
         li.appendChild(task);
 
         let remove = document.createElement('span');
         remove.classList.add('remove')
         remove.textContent = 'удалить';
         remove.addEventListener('click', function(e) {
-
-            console.log(e.target);
+            this.parentElement.remove();
         });
         li.appendChild(remove);
 
         let mark = document.createElement('span');
         mark.classList.add('mark')
         mark.textContent = 'сделано';
+        mark.addEventListener('click', function() {
+           task.classList.add('done')
+           //  this.parentElement.classList.add('done');
+        });
         li.appendChild(mark);
 
 
