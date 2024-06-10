7e8373b8e95937a8cd18dc42929177da2ad7ac1b
Master <v.pulin@yahoo.com>
2024-06-10 21:50:18 +0500

    Начало положенно первое задание сделал

--- Practice_Book/5.Practice_Tests/js/script.js
+++ Practice_Book/5.Practice_Tests/js/script.js
@@ -1,2 +1,20 @@
 'use strict'
 
+let inp = document.querySelectorAll('.inp');
+let btn = document.querySelector('#sub');
+// let dataValue = inp.getAttribute('data-right');
+
+btn.addEventListener('click', () => {
+    for (let inpElement of inp) {
+        let dataValue = inpElement.getAttribute('data-right');
+
+        if (dataValue === inpElement.value) {
+            inpElement.classList.add('right')
+        } else if (dataValue !== inpElement.value) {
+            inpElement.classList.add('wrong')
+        }
+    }
+
+})
+
+
