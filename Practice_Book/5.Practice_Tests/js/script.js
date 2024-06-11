32ce6e63656606c7ed5337897ddcad7c8437b54c
Master <v.pulin@yahoo.com>
2024-06-11 15:50:41 +0500

    Начинаю третий вариант задачи

--- Practice_Book/5.Practice_Tests/js/script.js
+++ Practice_Book/5.Practice_Tests/js/script.js
@@ -1,20 +1,37 @@
 'use strict'
 
+// Первый вариант
+// let inp = document.querySelectorAll('.inp');
+// let btn = document.querySelector('#sub');
+//
+// btn.addEventListener('click', () => {
+//     for (let inpElement of inp) {
+//         let dataValue = inpElement.getAttribute('data-right');
+//         if (dataValue === inpElement.value) {
+//             inpElement.classList.add('right')
+//         } else if (dataValue !== inpElement.value) {
+//             inpElement.classList.add('wrong')
+//         }
+//     }
+// })
+
+// Второй вариант
 let inp = document.querySelectorAll('.inp');
 let btn = document.querySelector('#sub');
-// let dataValue = inp.getAttribute('data-right');
-
+let answers = ['90', '4', '360',];
 btn.addEventListener('click', () => {
-    for (let inpElement of inp) {
-        let dataValue = inpElement.getAttribute('data-right');
-
-        if (dataValue === inpElement.value) {
-            inpElement.classList.add('right')
-        } else if (dataValue !== inpElement.value) {
-            inpElement.classList.add('wrong')
+    inp.forEach((input, index) => {
+        if (answers[index] === input.value) {
+            input.classList.add('right');
+            input.classList.remove('wrong');
+        } else {
+            input.classList.add('wrong');
+            input.classList.remove('right');
         }
-    }
+    });
+});
 
-})
+// Третий вариантё 
+
 
 
