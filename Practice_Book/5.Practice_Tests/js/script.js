de9c0cb7fe8eeb21a3d160ba7728d98c860e5d29
MS_ubuntu <v.pulin@yahoo.com>
2024-06-11 22:05:30 +0500

    Третий вариант тестов сделан

--- Practice_Book/5.Practice_Tests/js/script.js
+++ Practice_Book/5.Practice_Tests/js/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Первый вариант
+// ! Первый вариант
 // let inp = document.querySelectorAll('.inp');
 // let btn = document.querySelector('#sub');
 //
@@ -15,11 +15,51 @@
 //     }
 // })
 
-// Второй вариант
-let inp = document.querySelectorAll('.inp');
+// ! Второй вариант
+// let inp = document.querySelectorAll('.inp');
+// let btn = document.querySelector('#sub');
+// let answers = ['90', '4', '360',];
+// ];
+// btn.addEventListener('click', () => {
+//     inp.forEach((input, index) => {
+//         if (answers[index] === input.value) {
+//             input.classList.add('right');
+//             input.classList.remove('wrong');
+//         } else {
+//             input.classList.add('wrong');
+//             input.classList.remove('right');
+//         }
+//     });
+// });
+
+// ! Третий вариантё
+
 let btn = document.querySelector('#sub');
+let main = document.querySelector('#test');
+
 let answers = ['90', '4', '360',];
+let questions = ['Сколько градусов имеет угол квадрата?', 'Сколько углов у квадрата?', 'Сумма градусов углов квадрата?'];
+
+// Создаем вопросы и поля ввода
+questions.forEach((question, index) => {
+    let div = document.createElement('div');
+    let par = document.createElement('p');
+    let input = document.createElement('input');
+
+    // Добавляем элементы в DOM
+    main.appendChild(div);
+    div.appendChild(par);
+    par.textContent = question;
+    div.appendChild(input);
+
+    // Настраиваем поле ввода
+    input.placeholder = 'Введите ответы цифрами';
+    input.classList.add('inp');
+})
 btn.addEventListener('click', () => {
+    let inp = document.querySelectorAll('.inp');
+
+    // Проверяем ответы
     inp.forEach((input, index) => {
         if (answers[index] === input.value) {
             input.classList.add('right');
@@ -31,7 +71,9 @@ btn.addEventListener('click', () => {
     });
 });
 
-// Третий вариантё
+
+
+
 
 
 
